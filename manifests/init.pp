09ed297023c7435a46f7d9b7d9eac5cc028035f0
Sam Morrison <sam@vpac.org>
2012-02-10 13:25:30 +1100

    Initial import

---
+++ manifests/init.pp
@@ -0,0 +1,57 @@
+class openvpn {
+	package {
+		[ openvpn ] : ensure => latest;
+	}
+}
+
+class openvpn::client inherits openvpn {
+	
+	file {
+		client-conf:
+			path => "/etc/openvpn/client.conf",
+			owner => root,
+			group => root,
+			backup => false,
+			mode => 644,
+			require => Package["openvpn"],
+			content => template("openvpn/client.conf.erb");
+	}
+
+	service {
+		openvpn:
+			ensure => running,
+			enable => true,
+			hasrestart => true,
+			hasstatus => true,
+			status => "true",
+			require => Package["openvpn"],
+			subscribe => [ File[client-conf],Package["openvpn"]]
+	}
+}
+
+class openvpn::server inherits openvpn {
+# these two need to be passed different options
+# not sure the best way to make this happen.
+	file {
+		server-conf:
+			path => "/etc/openvpn/server.conf",
+			owner => root,
+			group => root,
+			backup => false,
+			mode => 644,
+			require => Package["openvpn"],
+			content => template("openvpn/server.conf.erb");
+	}
+
+	file {
+		server-conf:
+			path => "/etc/openvpn/monitor.conf",
+			owner => root,
+			group => root,
+			backup => false,
+			mode => 644,
+			require => Package["openvpn"],
+			content => template("openvpn/server.conf.erb");
+	}
+	
+}
