3494b620e5a0b24ae74be23aa34d75ad81df7fa1
Andrew Spiers <andrew@andrewspiers.net>
2016-11-24 10:49:44 +1100

    Keep vpn running under 16.04
    Openvpn under ubuntu 16.04 defines each vpn as a separate service.
    Merely running 'systemctl restart openvpn' will not start each vpn.
    
    With this change, the vpn connection called 'client' will be kept
    running.
    
    Change-Id: I3c231826eb44d9c46523252995ee8bded0162e20

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,7 +1,17 @@
+#instantiate an instance of a vpn
 class openvpn::client($server) inherits openvpn {
 
   openvpn::vpnclient {'default':
     o_remote => $server,
   }
 
+
+  case $::lsbdistcodename {
+    'xenial': {
+      service {'openvpn@client':
+        ensure => running,
+      }
+    }
+    default: { }
+  }
 }
